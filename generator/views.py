09ca46e4539a6b5985687c6e3d98f7095da09a89
Juan Mateos <juan.a.mateos@outlook.com>
2022-04-12 23:50:33 -0700

    first commit

---
+++ generator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def pokedex(request):
+    pokemon = request.GET.get('pokemon')
+    endpoint = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    res = requests.get(endpoint).json()
+    game_name = res['name']
+    game_index = res['game_indices'][3]['game_index']
+    game_type = res['types'][0]['type']['name']
+    game_weight = res['weight']
+    game_height = res['height']
+    game_sprit_front = res['sprites']['front_default']
+    game_sprit_back = res['sprites']['back_default']
+
+    return render(request, 'generator/pokedex.html', {'name': game_name, 'index':game_index,'type':game_type,'weight':game_weight,'height':game_height,'sprit_front_url':game_sprit_front,'sprit_back_url':game_sprit_back})
